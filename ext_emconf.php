12eb3626f3cfb6936e2f646f5060e679e7c44da7
Wolfgang Klinger <wk@plan2.net>
2018-12-18 10:45:16 +0100

    [TASK] Set compatibility to 8.7-9.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,31 +10,31 @@
  * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array (
-	'title' => 'Server side hyphenation for TYPO3',
-	'description' => 'Adds soft hyphenation capabilities to TYPO3. Provides a ViewHelper, a stdWrap property and some static templates to enable hyphenation.',
-	'category' => 'fe',
-	'version' => '3.0.4',
-	'state' => 'beta',
-	'uploadfolder' => 1,
-	'createDirs' => 'uploads/tx_nkhyphenation/',
-	'clearcacheonload' => 0,
-	'author' => 'Jost Baron',
-	'author_email' => 'j.baron@netzkoenig.de',
-	'author_company' => 'Netzkönig GbR',
-	'constraints' => 
-	array (
-		'depends' => 
-		array (
-			'php' => '7.0.0-7.1.99',
-			'typo3' => '8.7.10-8.7.99',
-		),
-		'conflicts' => 
-		array (
-		),
-		'suggests' => 
-		array (
-		),
-	),
-);
+$EM_CONF['nkhyphenation'] = [
+    'title' => 'Server side hyphenation for TYPO3',
+    'description' => 'Adds soft hyphenation capabilities to TYPO3. Provides a ViewHelper, a stdWrap property and some static templates to enable hyphenation.',
+    'category' => 'fe',
+    'version' => '4.0.0',
+    'state' => 'beta',
+    'uploadfolder' => 1,
+    'createDirs' => 'uploads/tx_nkhyphenation/',
+    'clearcacheonload' => 0,
+    'author' => 'Jost Baron',
+    'author_email' => 'j.baron@netzkoenig.de',
+    'author_company' => 'Netzkönig GbR',
+    'constraints' =>
+        [
+            'depends' =>
+                [
+                    'php' => '7.0.0-7.1.99',
+                    'typo3' => '8.7.10-9.5.99',
+                ],
+            'conflicts' =>
+                [
+                ],
+            'suggests' =>
+                [
+                ],
+        ],
+];
 
